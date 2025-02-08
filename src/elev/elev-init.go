494931282409ca7811265ce205e8683db1a89239
willisk0612 <willisk@stud.ntnu.no>
2025-02-08 14:21:55 +0100

    feat: structured logging

--- src/elev/elev-init.go
+++ src/elev/elev-init.go
@@ -1,10 +1,21 @@
 package elev
 
 import (
+	"log/slog"
 	"main/lib/driver-go/elevio"
 	"main/src/config"
 	"main/src/types"
+	"os"
 )
+
+// InitLogger sets up global logging configuration
+func InitLogger() {
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+}
 
 func InitElevator(nodeID int) *types.Elevator {
 	elevator := &types.Elevator{
@@ -13,17 +24,19 @@ func InitElevator(nodeID int) *types.Elevator {
 		Orders:    [config.N_FLOORS][config.N_BUTTONS]int{},
 		Behaviour: elevio.Idle,
 	}
+	slog.Debug("Elevator initialized", "nodeID", nodeID)
 	return elevator
 }
 
 func InitSystem(nodeID int) *types.Elevator {
 	elevator := InitElevator(nodeID)
-
 	if elevio.GetFloor() == -1 {
 		elevator.Dir = elevio.MD_Down
-		elevator.Behaviour = elevio.Moving
-		elevio.SetMotorDirection(elevator.Dir)
+		if err := moveElev(elevator); err == nil {
+			slog.Debug("Moving to known floor", "direction", "down")
+		} else {
+			slog.Error("Failed to start initial movement", "error", err)
+		}
 	}
-
 	return elevator
 }
