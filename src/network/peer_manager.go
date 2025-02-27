9e6408c7de8ae92eac78a63a9ee97aeb24af6e04
willisk0612 <willisk@stud.ntnu.no>
2025-02-27 18:30:10 +0100

    fix: mutex protected peer manager

--- src/network/peer_manager.go
+++ src/network/peer_manager.go
@@ -2,42 +2,43 @@ package network
 
 import (
 	"log/slog"
+	"sync"
 
 	"multivator/src/types"
 )
 
 var (
 	peerUpdatesChan = make(chan []string)
-	getPeersChan    = make(chan chan []string)
+	currentPeers    []string
+	peersMutex      sync.RWMutex
 )
 
 func peerManager() {
-	var currentPeers []string
-	for {
-		select {
-		case newList := <-peerUpdatesChan:
-			currentPeers = newList
-		case replyChan := <-getPeersChan:
-			replyChan <- currentPeers
-		}
+	for update := range peerUpdatesChan {
+		peersMutex.Lock()
+		currentPeers = update
+		peersMutex.Unlock()
 	}
 }
 
 func getCurrentPeers() []string {
-	reply := make(chan []string)
-	getPeersChan <- reply
-	return <-reply
+	peersMutex.RLock()
+	defer peersMutex.RUnlock()
+	result := make([]string, len(currentPeers))
+	copy(result, currentPeers)
+	return result
 }
 
 func handlePeerUpdates(peerUpdateCh <-chan types.PeerUpdate) {
 	for update := range peerUpdateCh {
-		peerUpdatesChan <- update.Peers
 		if update.New != "" {
-			slog.Info("New peer connected", "newPeer", update.New, "totalPeers", len(update.Peers), "allPeers", update.Peers)
+			slog.Info("New peer connected", "newPeer", update.New, "totalPeers", len(update.Peers))
 		}
 		if len(update.Lost) > 0 {
-			slog.Info("Peer(s) lost", "lostPeers", update.Lost, "totalPeers", len(update.Peers), "allPeers", update.Peers)
+			slog.Info("Peer(s) lost", "lostPeers", update.Lost, "totalPeers", len(update.Peers))
 		}
 		slog.Info("Current peer list updated", "count", len(update.Peers), "peers", update.Peers)
+
+		peerUpdatesChan <- update.Peers
 	}
 }
