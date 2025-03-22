3177a3bc3b44f78963ee5a905c6f74bdaef49ff6
willisk0612 <willisk@stud.ntnu.no>
2025-03-22 17:01:36 +0100

    fix: better msgBufferRx

--- src/dispatcher/msg_buffer.go
+++ src/dispatcher/msg_buffer.go
@@ -27,15 +27,13 @@ func msgBufferTx[T MsgContent](msgBufTxCh chan Msg[T], msgTxCh chan Msg[T], atom
 func msgBufferRx[T MsgContent](msgBufRxCh chan Msg[T], msgRxCh chan Msg[T], atomicCounter *atomic.Uint64) {
 	seenMsgs := make(map[string]bool)
 	recentMsgIDs := make([]string, config.MsgRepetitions)
-	var nextIndex atomic.Uint32
+	var nextIndex int
 
 	for msgRx := range msgRxCh {
 		if msgRx.SenderID != config.NodeID {
 			msgID := fmt.Sprintf("%d-%d", msgRx.SenderID, msgRx.Counter)
-
 			if !seenMsgs[msgID] {
 				seenMsgs[msgID] = true
-
 				// Update Lamport timestamp
 				for {
 					localTime := atomicCounter.Load()
@@ -46,12 +44,14 @@ func msgBufferRx[T MsgContent](msgBufRxCh chan Msg[T], msgRxCh chan Msg[T], atom
 				}
 
 				// Delete old message from seen messages
-				index := int((nextIndex.Add(1) - 1) % config.MsgRepetitions)
+				index := nextIndex
 				oldMsgID := recentMsgIDs[index]
 				if oldMsgID != "" {
 					delete(seenMsgs, oldMsgID)
 				}
 				recentMsgIDs[index] = msgID
+				nextIndex = (nextIndex + 1) % config.MsgRepetitions
+
 				msgBufRxCh <- msgRx
 			}
 		}
